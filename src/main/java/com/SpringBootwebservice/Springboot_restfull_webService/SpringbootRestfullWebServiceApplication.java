e7cb1e916ab8a649bfcff80802bfe29298fdc107
SANTHOSH KUMAR M <msksanthosh2000@gmail.com>
2024-08-24 22:11:52 +0530

    Initial commit

---
+++ src/main/java/com/SpringBootwebservice/Springboot_restfull_webService/SpringbootRestfullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootwebservice.Springboot_restfull_webService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootwebservice/Springboot_restfull_webService/SpringbootRestfullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootwebservice.Springboot_restfull_webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfullWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
